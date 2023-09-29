0acea2ec40d1ea6384a923936cf9a235140266de
RadziSzop <radek.krochmall@gmail.com>
2023-09-29 14:00:00 +0200

    added update profile functionality

--- src/routes/profile/api.ts
+++ src/routes/profile/api.ts
@@ -1,8 +1,20 @@
 import "../../auth/OIDCStrategy";
 import { Router } from "express";
 import { authenticate } from "../../middlewares/authenticate";
-import { getProfile } from "./profile-route";
+import { getProfile, updateProfile } from "./profile-route";
+import { body } from "express-validator";
+import { validateBody } from "../../utils/validateBody";
 
 export const profileRouter = Router();
 
 profileRouter.get("/", authenticate, getProfile);
+profileRouter.put(
+  "/",
+  authenticate,
+  body("phoneNumber")
+    .isMobilePhone("any")
+    .withMessage("Niepoprawny numer telefonu|201"),
+  body("contactEmail").isEmail().withMessage("Niepoprawny adres email|202"),
+  validateBody,
+  updateProfile
+);
--- src/routes/profile/profile-route.ts
+++ src/routes/profile/profile-route.ts
@@ -21,3 +21,30 @@ export const getProfile = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const updateProfile = async (req: Request, res: Response) => {
+  try {
+    const { contactEmail, phoneNumber } = req.body as {
+      contactEmail: string;
+      phoneNumber: string;
+    };
+    const user = req.user as number;
+    await prisma.profile.update({
+      where: {
+        userId: user,
+      },
+      data: {
+        contact_email: contactEmail,
+        phone_number: phoneNumber,
+      },
+    });
+    res.json(user);
+  } catch (error) {
+    console.log("get profile error", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+      errorCode: 199,
+    });
+  }
+};
